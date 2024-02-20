7da148dfd31c8dbc7a21a25e81373c02ebabc9e6
Jorge Alfonso Luqueño Díaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2024-02-20 10:27:03 +0000

    Guardado nuevo código del codespace

--- aplicacion_02/mvc/controllers/index.py
+++ aplicacion_02/mvc/controllers/index.py
@@ -5,7 +5,7 @@ render = web.template.render('mvc/views/')
 class Index:
     def GET(self):
         try:
-            return render.inde()
+            return render.index()
         except Exception as e:
             print(f"Error 101 - index {e.args}")
             return "Oops! \n Algo salió mal."
---
+++ aplicacion_02/mvc/models/modelo_productos.py
@@ -0,0 +1,11 @@
+class ModeloProductos:
+    def listaProductos(self):
+        #Código aquí
+    def insertarProducto (self, producto):
+        #Código aquí
+    def borrarProducto (self, id_producto):
+        #Código aquí
+    def detalleProducto (self, id_producto):
+        #Código aquí
+    def actualizarProducto (self, producto):
+        #Código
