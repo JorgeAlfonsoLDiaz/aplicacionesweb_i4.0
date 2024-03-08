fac9677319821e532c6e2bc2e42eff40d24b4873
JorgeAlfonsoLDiaz <1722110483@utectulancingo.edu.mx>
2024-03-08 11:16:33 -0600

    Se corrigió un error en el controlador index.py

--- api_crud/mvc/controllers/index.py
+++ api_crud/mvc/controllers/index.py
@@ -4,7 +4,7 @@ from models.modelo_productos import Producto
 render = web.template.render('views/')
 modelo_producto = Producto()
 
-class Listar:
+class Index:
     def GET(self):
         productos = producto.obtener_productos()
             return render.index(productos)
