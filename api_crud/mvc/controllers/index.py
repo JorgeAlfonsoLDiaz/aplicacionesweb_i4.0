4a6d76b25444b6cdf701c9092494148d8e84de49
JorgeAlfonsoLDiaz <1722110483@utectulancingo.edu.mx>
2024-03-08 11:48:16 -0600

    Se corrigió un error de indentación en index.py, línea 10

--- api_crud/mvc/controllers/index.py
+++ api_crud/mvc/controllers/index.py
@@ -7,5 +7,5 @@ modelo_producto = Producto()
 class Index:
     def GET(self):
         productos = producto.obtener_productos()
-            return render.index(productos)
+        return render.index(productos)
         
