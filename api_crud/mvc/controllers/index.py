910e5a4096b2e3be144064b2c10b69d70b4e1117
Jorge Alfonso Luqueño Díaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2024-03-08 18:06:40 +0000

    La prueba mostró resultados satisfactorios: Los productos ahora se listan correctamente en la tabla. Nota: Hay un problema encontrando los diseños.

--- api_crud/mvc/controllers/index.py
+++ api_crud/mvc/controllers/index.py
@@ -1,11 +1,11 @@
 import web
-from models.modelo_productos import Producto
+from mvc.models.modelo_productos import Producto
 
 render = web.template.render('mvc/views/')
 modelo_producto = Producto()
 
 class Index:
     def GET(self):
-        productos = producto.obtener_productos()
-            return render.index(productos)
+        productos = modelo_producto.obtener_productos()
+        return render.index(productos)
         
